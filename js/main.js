3533b704b9416e022335f267cb05f175a2ad0e27
MagedGhabour <92618945+Maged-Ghabour@users.noreply.github.com>
2023-09-06 11:08:17 +0300

    v1

---
+++ js/main.js
@@ -0,0 +1,40 @@
+/* Swipper  */
+var s1 = new Swiper(".s1", {
+  slidesPerView: 6,
+  spaceBetween: 20,
+  sliderPerGroup: 6,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  
+  pagination: {
+    el: ".swiper-pagination1",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 5,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
